f26430e65f6d8babe9a9230ade2fe6c556af02f8
unknown <tupnv24@gmail.com>
2023-06-09 16:52:19 +0700

    tu/create-database

--- database/migrations/2023_06_09_090939_users.php
+++ database/migrations/2023_06_09_090939_users.php
@@ -12,12 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('user_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone');
+            $table->string('email');
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('isActive');
+            $table->unsignedInteger('role_id');
+            $table->foreign('role_id')->references('role_id')->on('roles');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2023_06_09_093448_shops.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->bigIncrements('shop_id');
+            $table->string('name');
+            $table->string('location');
+            $table->string('description');
+            $table->string('tag');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shops');
+    }
+};
