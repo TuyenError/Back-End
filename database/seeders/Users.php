b18f291ad7a08dd50b5111656ce032fad92fba97
unknown <tupnv24@gmail.com>
2023-06-15 16:18:17 +0700

    edit table

--- database/seeders/Users.php
+++ database/seeders/Users.php
@@ -19,7 +19,6 @@ class Users extends Seeder
                 'name' => 'Hoàng Diệu',
                 'phone' => '09786 545 632',
                 'email' => 'hoangdieu@gmail.com',
-                'username' => 'HoangDieu',
                 'password' => 'HoangDieu123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -29,7 +28,6 @@ class Users extends Seeder
                 'name' => 'Nguyễn Du',
                 'phone' => '0944 362 782',
                 'email' => 'nguyendu@gmail.com',
-                'username' => 'NguyenDu',
                 'password' => 'Nguyendu555!',
                 'isActive' => true,
                 'role_id' => 2
@@ -39,7 +37,6 @@ class Users extends Seeder
                 'name' => 'Lê Đình Lý',
                 'phone' => '0993 263 487',
                 'email' => 'ledinhly@gmail.com',
-                'username' => 'LeDinhLy',
                 'password' => 'Ledinhly#@!111',
                 'isActive' => true,
                 'role_id' => 2
@@ -49,7 +46,6 @@ class Users extends Seeder
                 'name' => 'Hải Phòng',
                 'phone' => '0915 876 532',
                 'email' => 'haiphong@gmail.com',
-                'username' => 'HaiPhong',
                 'password' => 'Haiphong222#',
                 'isActive' => true,
                 'role_id' => 2
@@ -59,7 +55,6 @@ class Users extends Seeder
                 'name' => 'Bếp Tiên',
                 'phone' => '0987 645 344',
                 'email' => 'beptien@gmail.com',
-                'username' => 'BepTien',
                 'password' => 'Beptien54@',
                 'isActive' => true,
                 'role_id' => 2
@@ -69,8 +64,7 @@ class Users extends Seeder
                 'name' => 'Ngô Quyền',
                 'phone' => '0945 227 639',
                 'email' => 'ngoquyen@gmail.com',
-                'username' => 'ngoquyen',
-                'password' => 'ngoquyen123#@!',
+                    'password' => 'ngoquyen123#@!',
                 'isActive' => true,
                 'role_id' => 2
             ],
@@ -79,7 +73,6 @@ class Users extends Seeder
                 'name' => 'Hà Huy Tập',
                 'phone' => '0912 378 663',
                 'email' => 'hahuytap@gmail.com',
-                'username' => 'hahuytap',
                 'password' => 'hahuytap123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -89,7 +82,6 @@ class Users extends Seeder
                 'name' => 'Trịnh Công Sơn',
                 'phone' => '0976 000 356',
                 'email' => 'trinhcongson@gmail.com',
-                'username' => 'trinhcongson',
                 'password' => 'trinhcongson123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -99,7 +91,6 @@ class Users extends Seeder
                 'name' => 'My Love',
                 'phone' => '0945 550 072',
                 'email' => 'mylove@gmail.com',
-                'username' => 'mylove',
                 'password' => 'mylove123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -109,7 +100,6 @@ class Users extends Seeder
                 'name' => 'Mai',
                 'phone' => '0989 955 502',
                 'email' => 'mai@gmail.com',
-                'username' => 'mai',
                 'password' => 'mai123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -119,7 +109,6 @@ class Users extends Seeder
                 'name' => 'Lâm Nhật',
                 'phone' => '0956 370 088',
                 'email' => 'lamnhat@gmail.com',
-                'username' => 'lamnhat',
                 'password' => 'lamnhat123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -129,7 +118,6 @@ class Users extends Seeder
                 'name' => 'Phúc Long',
                 'phone' => '0966 736 229',
                 'email' => 'phuclong@gmail.com',
-                'username' => 'phuclong',
                 'password' => 'phuclong123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -139,7 +127,6 @@ class Users extends Seeder
                 'name' => 'Nguyễn Văn Linh',
                 'phone' => '0988 222 002',
                 'email' => 'nguyenvanlinh@gmail.com',
-                'username' => 'nguyenvanlinh',
                 'password' => 'nguyenvanlinh123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -149,7 +136,6 @@ class Users extends Seeder
                 'name' => 'Điện Biên Phủ',
                 'phone' => '0944 466 601',
                 'email' => 'dienbienphu@gmail.com',
-                'username' => 'dienbienphu',
                 'password' => 'dienbienphu123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -159,7 +145,6 @@ class Users extends Seeder
                 'name' => 'Ngô Gia Tự',
                 'phone' => '0999 500 789',
                 'email' => 'ngogiatu@gmail.com',
-                'username' => 'ngogiatu',
                 'password' => 'ngogiatu123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -169,7 +154,6 @@ class Users extends Seeder
                 'name' => 'Hoàng Dư Khương',
                 'phone' => '0988 311 122',
                 'email' => 'hoangdukhuong@gmail.com',
-                'username' => 'hoangdukhuong',
                 'password' => 'hoangdukhuong123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -179,7 +163,6 @@ class Users extends Seeder
                 'name' => 'Mẹ Thứ',
                 'phone' => '0977 826 662',
                 'email' => 'methu@gmail.com',
-                'username' => 'methu',
                 'password' => 'methu123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -189,7 +172,6 @@ class Users extends Seeder
                 'name' => 'Bông',
                 'phone' => '0977 700 445',
                 'email' => 'bong@gmail.com',
-                'username' => 'bong',
                 'password' => 'bong123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -199,7 +181,6 @@ class Users extends Seeder
                 'name' => 'Phạm Văn Đồng',
                 'phone' => '0966 073 301',
                 'email' => 'phamvandong@gmail.com',
-                'username' => 'phamvandong',
                 'password' => 'phamvandong123#@!',
                 'isActive' => true,
                 'role_id' => 2
@@ -209,7 +190,6 @@ class Users extends Seeder
                 'name' => 'Shop Online',
                 'phone' => '0955 888 922',
                 'email' => 'shoponline@gmail.com',
-                'username' => 'shoponline',
                 'password' => 'shoponline12345!',
                 'isActive' => true,
                 'role_id' => 2
